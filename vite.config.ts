3ebeda85939124eed01339e9b7ef8a76581b9a53
thisisplanetdev <thisislily01992@gmail.com>
2025-11-14 15:20:21 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-
-    // ➜ ADD THIS
-    allowedHosts: [
-      "vedhanth-portfolio.onrender.com"
-    ],
+    allowedHosts: ["vedhanth-portfolio.onrender.com"],
   },
-
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
